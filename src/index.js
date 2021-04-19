fcda39380409a170c37988be4d639a0b479f6d4a
Koen Haagsma <koenhaagsma123@hotmail.nl>
2021-04-19 13:38:06 +0200

    Setting up Basic routing w/ pug

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const chalk = require("chalk");
 const app = express();
 const port = 3001;
 
+app.set("view engine", "pug");
+app.set("views", "./views");
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
