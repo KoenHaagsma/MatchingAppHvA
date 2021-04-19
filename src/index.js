43443b559a606ecf930b13714b990ceb0f4069d4
Koen Haagsma <koenhaagsma123@hotmail.nl>
2021-04-19 13:54:00 +0200

    Setting up Pug + initializing pug pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
+// Requirements
 const express = require("express");
 const chalk = require("chalk");
+const pug = require("pug");
+
+//Using path, doesn't have to be imported to use.
+const path = require("path");
+
+// Init app
 const app = express();
 const port = 3001;
 
+//Load view engine | Path: Directory name + map name.
+app.set(
+    "views",
+    path.join(__dirname, "views")
+);
 app.set("view engine", "pug");
-app.set("views", "./views");
 
+// Home Route
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.render("index");
 });
 
+// Start Server
 app.listen(port, () => {
     console.log(
         chalk.red(
